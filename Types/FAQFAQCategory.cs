661ef7e838172c8579d437ea22fcee8ac3dbd0a4
AutoCode <autocode@papergo.io>
2024-08-01 19:23:21 +0300

    update from autocode on 2024-08-01 19:23:21.094828

--- Types/FAQFAQCategory.cs
+++ Types/FAQFAQCategory.cs
@@ -1,12 +1,10 @@
 
-using System;
-
 namespace ProjectName.Types
 {
     public class FAQFAQCategory
     {
-        public Guid Id { get; set; }
-        public Guid FAQId { get; set; }
-        public Guid FAQCategoryId { get; set; }
+        public Guid? Id { get; set; }
+        public Guid? FAQId { get; set; }
+        public Guid? FAQCategoryId { get; set; }
     }
 }
